1d37da9a18db4e9da0b7861ca9a73403487f7bdf
Zane Daniel <118499857+ZaneDaniel11@users.noreply.github.com>
2024-06-20 00:32:21 +0800

    Destructuring
    Destructuring props

--- reactpro/src/components/Hello.jsx
+++ reactpro/src/components/Hello.jsx
@@ -2,9 +2,13 @@
 //   return "Testing";
 // }
 function Hello(props) {
+  // You can also destructure a prop on Function
+  // Destructuring props
+  // const { name, greatings } = props;
   return (
     <div>
       <h1>
+        {/* {name} {greatings} */}
         {props.greatings} {props.name}
       </h1>
     </div>
