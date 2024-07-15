c0cc00d35bbbb9b50bd335f97ae20f87d027ab35
Zane Daniel <118499857+ZaneDaniel11@users.noreply.github.com>
2024-07-15 23:12:37 +0800

    Handle InpuT Form

--- reactpro/src/App.jsx
+++ reactpro/src/App.jsx
@@ -1,10 +1,11 @@
+import Form from "./components/Form";
 import Message from "./components/Message";
 import Fdata from "./components/fetch";
 
 function App() {
   return (
     <div className="App">
-      <Fdata />
+      <Form />
     </div>
   );
 }
---
+++ reactpro/src/components/Form.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+export default function Form() {
+  const [data, Formdate] = useState("");
+
+  function Data(e) {
+    Formdate(e.target.value);
+  }
+  console.log(data);
+  return (
+    <div>
+      <form>
+        <input
+          onChange={(e) => Formdate(e.target.value)}
+          type="text"
+          value={data}
+        />
+      </form>
+    </div>
+  );
+}
